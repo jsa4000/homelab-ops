241034e1f10ab3ddb7e96ff6a56636e216116403
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2024-05-30 08:44:54 +0200

    Added more files

---
+++ kubernetes/apps/media/qbittorrent/app/media-init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ROOT_FOLDER=/downloads
+
+FOLDERS=( "movies" "series" "deleted" "tmp")
+
+printf "Creating folder structure for media.\n"
+
+for FOLDER in "${FOLDERS[@]}"; do
+  printf "Creating folder: %s\n" $ROOT_FOLDER/$FOLDER
+  mkdir -p $ROOT_FOLDER/$FOLDER
+done
+
+printf "Folders created.\n"
