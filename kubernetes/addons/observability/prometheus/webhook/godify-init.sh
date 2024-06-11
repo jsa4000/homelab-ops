d0acead41f88d5ec71d51a9eb018f070ccc912e8
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2024-06-11 20:42:59 +0200

    Added more files

--- kubernetes/addons/observability/prometheus/webhook/godify-init.sh
+++ kubernetes/addons/observability/prometheus/webhook/godify-init.sh
@@ -14,6 +14,14 @@ apk add curl jq
 echo "Wait connecting to Gotify..."
 while [ "$(curl -sk -o /dev/null -w '%{http_code}' $SCHEME_URL://$GOTIFY_URL$PORT$GOTIFY_HEALTH_ENDPOINT)" -ne 200 ]; do sleep 10; done
 
+echo "Check if application has been already created"
+RESULT=$(curl -k -s -u $GOTIFY_USER_NAME:$GOTIFY_USER_PASS $SCHEME_URL://$GOTIFY_URL$PORT$GOTIFY_APPICATION_ENDPOINT | jq --arg GOTIFY_APPLICATION_NAME $GOTIFY_APPLICATION_NAME '.[] | select(.name | test($GOTIFY_APPLICATION_NAME)?) | length')
+if [ ! -z "$RESULT" ]; then
+  echo "Application $GOTIFY_APPLICATION_NAME already created";
+  return 0;
+fi
+
+echo "Creating application $GOTIFY_APPLICATION_NAME"
 curl -k -s -u $GOTIFY_USER_NAME:$GOTIFY_USER_PASS $SCHEME_URL://$GOTIFY_URL$PORT$GOTIFY_APPICATION_ENDPOINT \
 -F "name=$GOTIFY_APPLICATION_NAME" \
 -F "description=$GOTIFY_APPLICATION_DESCRIPTION" \
