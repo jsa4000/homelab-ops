f83a8ca03c7868802915e75ae60de694c7e88e3c
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2024-06-11 22:25:34 +0200

    Added more files

--- kubernetes/addons/observability/prometheus/webhook/godify-init.sh
+++ kubernetes/addons/observability/prometheus/webhook/godify-init.sh
@@ -6,9 +6,12 @@ GOTIFY_URL=gotify.observability.svc.cluster.local
 GOTIFY_PORT=80
 GOTIFY_HEALTH_ENDPOINT=/health
 GOTIFY_APPICATION_ENDPOINT=/application
+GOTIFY_TOKEN_ENV=GOTIFY_TOKEN
 CONFIG_FOLDER=/config
+OUTPUT_FILE=.env
 
 # Install utils
+# TODO: Use custom immutable image
 apk add curl jq
 
 echo "Wait connecting to Gotify..."
@@ -25,6 +28,9 @@ echo "Creating application $GOTIFY_APPLICATION_NAME"
 curl -k -s -u $GOTIFY_USER_NAME:$GOTIFY_USER_PASS $SCHEME_URL://$GOTIFY_URL$PORT$GOTIFY_APPICATION_ENDPOINT \
 -F "name=$GOTIFY_APPLICATION_NAME" \
 -F "description=$GOTIFY_APPLICATION_DESCRIPTION" \
-| jq '.token' > $CONFIG_FOLDER/GOTIFY_TOKEN
+| echo "GOTIFY_TOKEN_ENV=$(jq '.token') > $CONFIG_FOLDER/$OUTPUT_FILE
+
+# Debug
+cat $CONFIG_FOLDER/$OUTPUT_FILE
 
 printf "Gotify initialized created.\n"
