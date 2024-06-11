899807e045a4722d7baaa1cbb87eece8ee0fe783
Javier Santos Andrés <jsantosa@MacBook-Pro.local>
2024-06-11 16:20:04 +0200

    Added more files

---
+++ kubernetes/addons/observability/prometheus/webhook/godify-init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Variables
+SCHEME_URL=http
+GOTIFY_URL=gotify.observability.svc.cluster.local
+GOTIFY_PORT=80
+GOTIFY_HEALTH_ENDPOINT=/health
+GOTIFY_APPICATION_ENDPOINT=/application
+
+GOTIFY_APPLICATION_NAME=test03
+GOTIFY_APPLICATION_DESCRIPTION=tutorial
+
+# Install utils
+apk add curl jq
+
+echo "Wait connecting to Gotify..."
+while [ "$(curl -sk -o /dev/null -w '%{http_code}' $SCHEME_URL://$GOTIFY_URL$PORT$GOTIFY_HEALTH_ENDPOINT)" -ne 200 ]; do sleep 10; done
+
+curl -k -u $GOTIFY_USER_NAME:$GOTIFY_USER_PASS $SCHEME_URL://$GOTIFY_URL$PORT$GOTIFY_APPICATION_ENDPOINT \
+-F "name=$GOTIFY_APPLICATION_NAME" \
+-F "description=$GOTIFY_APPLICATION_DESCRIPTION" \
+| jq '.token'
+
+printf "Gotify initialized created.\n"
